c7caa95c16d2059fef05ad1510a7e455d4b1534b
jhkong <jhkong@srdnetworks.com>
2019-08-06 15:59:30 +0900

    no message

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,49 +4,49 @@ module.exports = function(app) {
 
   app.use(
     proxy('/web/rec/api/scm', { 
-      target: 'http://algozip.co.kr:8001', 
+      target: 'http://localhost:8001', 
       changeOrigin: true
     })
   );
   app.use(
     proxy('/web/rec/api/oms', { 
-      target: 'http://algozip.co.kr:8002', 
+      target: 'http://localhost:8002', 
       changeOrigin: true
     })
   );
   app.use(
     proxy('/web/rec/api/bms', {
-      target: 'http://algozip.co.kr:8003',
+      target: 'http://localhost:8003',
       changeOrigin: true
     })
   );
   app.use(
     proxy('/web/rec/api/mpa', {
-      target: 'http://algozip.co.kr:8004',
+      target: 'http://localhost:8004',
       changeOrigin: true
     })
   );
   app.use(
     proxy('/web/rec/api/blog', {
-      target: 'http://algozip.co.kr:8005',
+      target: 'http://localhost:8005',
       changeOrigin: true
     })
   );
   app.use(
     proxy('/web/rec/api/file', {
-      target: 'http://algozip.co.kr:8006',
+      target: 'http://localhost:8006',
       changeOrigin: true
     })
   );
   app.use(
     proxy('/web/srd/common', {
-      target: 'http://algozip.co.kr:8007',
+      target: 'http://localhost:8007',
       changeOrigin: true
     })
   );
   app.use(
     proxy('/uaa', { 
-      target: 'http://algozip.co.kr:8000', 
+      target: 'http://localhost:8000', 
       changeOrigin: true
     })
   );
