5a8d0da5df6168025034ad6630b458950ee91ef3
jhkong <jhkong@srdnetworks.com>
2019-09-17 21:30:22 +0900

    no message

--- src/OAuth2Config.js
+++ src/OAuth2Config.js
@@ -1,16 +1,16 @@
 import config from './config';
 const ClientOAuth2 = require('client-oauth2')
 
-export const jwt = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmOHGvF+HJXmcR2InYw1DaYbCtiu7tAhdAE5UrG1+vu80hb3mSs/AKNLcImiW+lIPy9UoTxeLuKWkRIdBMrwoPEEcZ4t833M/Ij+FYjk73kMc/Spf9v48IoeLoHFoTsDhtwoldEhXdaBKckjbXrBiLw0gtNmFFpE9NP2mkSbdXryJo7yX5gVZNNThzbUl6zjcbh2LFIncDDYp4YdAnxHYbsVYwwAwDqNv9H3+VDO9UZhoBszfMcxyHt6PQKkg0hB24ZynCIKxS4mQe8MEmLlVx1YNL4yr0zc7Fm7yDQpR7QxNdML+ClPfU6lHRS2H15onYKv4ucC/ivCoEIt6sQ4FjwIDAQAB';
-export const oauth_scm = new ClientOAuth2({
-  body: {
-    grant_type: 'client_credentials',
-  },
-  clientId: 'scm',
-  clientSecret: 'scmsecret',
-  accessTokenUri: `${config.authService}/oauth/token`,
-  scopes: ['create', 'read', 'update', 'delete'],
-});
+// export const jwt = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmOHGvF+HJXmcR2InYw1DaYbCtiu7tAhdAE5UrG1+vu80hb3mSs/AKNLcImiW+lIPy9UoTxeLuKWkRIdBMrwoPEEcZ4t833M/Ij+FYjk73kMc/Spf9v48IoeLoHFoTsDhtwoldEhXdaBKckjbXrBiLw0gtNmFFpE9NP2mkSbdXryJo7yX5gVZNNThzbUl6zjcbh2LFIncDDYp4YdAnxHYbsVYwwAwDqNv9H3+VDO9UZhoBszfMcxyHt6PQKkg0hB24ZynCIKxS4mQe8MEmLlVx1YNL4yr0zc7Fm7yDQpR7QxNdML+ClPfU6lHRS2H15onYKv4ucC/ivCoEIt6sQ4FjwIDAQAB';
+// export const oauth_scm = new ClientOAuth2({
+//   body: {
+//     grant_type: 'client_credentials',
+//   },
+//   clientId: 'scm',
+//   clientSecret: 'scmsecret',
+//   accessTokenUri: `${config.authService}/oauth/token`,
+//   scopes: ['create', 'read', 'update', 'delete'],
+// });
 
 export const oauth_web = new ClientOAuth2({
   clientId: 'web',
